dfe9bb94a8be3ff1bad214f1b460ecd48b297a61
Raigyo <vincent.chilot@gmail.com>
2019-03-21 09:41:52 +0100

    Updates

---
+++ precache-manifest.a28d2fe98ceb90ebfbe89e444999d6e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "95793ce7f4beb3190fa3",
+    "url": "/tic-tac-toe/static/js/main.c46d3e05.chunk.js"
+  },
+  {
+    "revision": "0bd08c5f063516b7ff94",
+    "url": "/tic-tac-toe/static/js/2.0c9cd3e4.chunk.js"
+  },
+  {
+    "revision": "95793ce7f4beb3190fa3",
+    "url": "/tic-tac-toe/static/css/main.5790dbda.chunk.css"
+  },
+  {
+    "revision": "fccbec4d54de44ee0ae18daf949263ff",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
